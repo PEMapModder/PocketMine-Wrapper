920c1f9f2db79a98ada24765dda0813dc5df9af6
PEMapModder <pemapmodder1970@gmail.com>
2015-09-15 17:57:44 +0800

    Update wrapper.php

--- wrapper.php
+++ wrapper.php
@@ -37,12 +37,36 @@ $restarts = (int) $line;
 $restarts = ($restarts === 0) ? PHP_INT_MAX : $restarts;
 
 while($restarts--){
-	$server = proc_open(PHP_BINARY . " " . implode(" ", $args), [0 => ["pipe", "r"], 1 => ["pipe", ,"w"]], $pipes);
+	console("Initializing server...");
+	$server = proc_open(PHP_BINARY . " " . implode(" ", $args), [0 => ["pipe", "r"], 1 => ["pipe", ,"w"], 2 => fopen("php://stderr", "wb")], $pipes);
 	if(!is_resource($server)){
 		console("[ERROR] Failed to start server once :(");
 		continue;
 	}
-	while(proc_get_status($server)["running"] === false){
-		
+	console("[*] The server has been started. Type '-die' to stop the server and prevent this script from restarting. Type '-kill' to terminate the server and stop this script.");
+	while(proc_get_status($server)["running"]){
+		echo stream_get_contents($pipes[1]);
+		while(($line = nonBlockReadLine()) !== null){
+			if(strtolower($line) === "-die"){
+				$restarts = 0;
+				$line = "stop";
+			}elseif(strtolower($line) === "-kill"){
+				fclose($pipes[0]);
+				fclose($pipes[1]);
+				if(isset($pipes[2])){
+					fclose($pipes[2]);
+				}
+				proc_terminate($pipes[2]);
+			}
+			fwrite($pipes[0], $line . PHP_EOL);
+		}
 	}
+	echo stream_get_contents($pipes[1]);
+	fclose($pipes[1]);
+	fclose($pipes[0]);
+	if(isset($pipes[2])){ // I am not sure if it exists
+		fclose($pipes[2]);
+	}
+	proc_close($pipes[2]);
+	console("[*] The server has been stopped.");
 }
