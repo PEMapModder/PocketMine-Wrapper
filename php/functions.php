99bb36731998912fbedf0a0c8a43638caa4b1d5d
PEMapModder <pemapmodder1970@gmail.com>
2015-10-18 14:11:29 +0800

    Fixed

--- php/functions.php
+++ php/functions.php
@@ -1,17 +1,5 @@
 <?php
 
-function console($msg, $ret = null, $eol = true){
-	echo date("[H:i:s] ") . $msg . ($eol ? PHP_EOL : "");
-	return $ret;
-}
-
-$cmdReader = new CommandReader();
-
-function nonBlockReadLine($line){
-	global $cmdReader;
-	return $cmdReader->getLine();
-}
-
 /**
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -20,8 +8,9 @@ function nonBlockReadLine($line){
  *
  * @author PocketMine Team
  * @link http://www.pocketmine.net/
-*/
+ */
 
+/** @noinspection PhpMultipleClassesDeclarationsInOneFile */
 class CommandReader extends Thread{
 	public static $os;
 	private $readline;
@@ -96,10 +85,142 @@ class CommandReader extends Thread{
 				self::$os = "other";
 			}
 		}
-		
+
 		return self::$os;
 	}
 }
+
+/** @noinspection PhpMultipleClassesDeclarationsInOneFile */
+class ThreadedFactory{
+	/** @var ThreadedFactory */
+	protected static $instance;
+	/** @var \Threaded[] */
+	protected $threadedList = [];
+
+	protected function __construct(){
+		global $threadedFactoryInstance;
+		$threadedFactoryInstance = $this;
+		self::$instance = $this;
+	}
+
+	/**
+	 * @return ThreadedFactory
+	 */
+	public static function getInstance(){
+		if(self::$instance === null){
+			global $threadedFactoryInstance;
+			if($threadedFactoryInstance instanceof ThreadedFactory){
+				self::$instance = $threadedFactoryInstance;
+			}else{
+				new ThreadedFactory();
+			}
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * @param \Threaded $class
+	 * @param ...$arguments
+	 *
+	 * @return \Threaded
+	 */
+	public static function create($class = \Threaded::class, ...$arguments){
+		/** @var \Threaded $threaded */
+		$threaded = new $class(...$arguments);
+		self::getInstance()->threadedList[spl_object_hash($threaded)] = $threaded;
+		return $threaded;
+	}
+
+	public static function destroy(\Threaded $threaded){
+		$instance = self::getInstance();
+		if(isset($instance->threadedList[$hash = spl_object_hash($threaded)])){
+			$threaded->synchronized(function(\Threaded $t){
+				$t->notify();
+			}, $threaded);
+			unset($instance->threadedList[$hash]);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @return \Threaded[]
+	 */
+	public static function all(){
+		return self::getInstance()->threadedList;
+	}
+}
+
+/** @noinspection PhpMultipleClassesDeclarationsInOneFile */
+class StreamReader extends Thread{
+	public static $os;
+	/** @var \Threaded */
+	protected $buffer;
+	private $res;
+	public $running = true;
+	public function __construct($resource){
+		$this->buffer = \ThreadedFactory::create();
+		$this->res = $resource;
+		$this->start();
+	}
+	private function readLine(){
+		$line = rtrim(fgets($this->res), "\r\n");
+		return $line;
+	}
+	/**
+	 * Reads a line from console, if available. Returns null if not available
+	 *
+	 * @return string|null
+	 */
+	public function getLine(){
+		if($this->buffer->count() !== 0){
+			return $this->buffer->shift();
+		}
+		return null;
+	}
+	public function run(){
+		$lastLine = microtime(true);
+		while($this->running){
+			if(($line = $this->readLine()) !== ""){
+				$this->buffer[] = preg_replace("#\\x1b\\x5b([^\\x1b]*\\x7e|[\\x40-\\x50])#", "", $line);
+			}elseif((microtime(true) - $lastLine) <= 0.1){ //Non blocking! Sleep to save CPU
+				usleep(40000);
+			}
+			$lastLine = microtime(true);
+		}
+	}
+	public function getThreadName(){
+		return "Console";
+	}
+	public static function getOS($recalculate = false){
+		if(self::$os === null or $recalculate){
+			$uname = php_uname("s");
+			if(stripos($uname, "Darwin") !== false){
+				if(strpos(php_uname("m"), "iP") === 0){
+					self::$os = "ios";
+				}else{
+					self::$os = "mac";
+				}
+			}elseif(stripos($uname, "Win") !== false or $uname === "Msys"){
+				self::$os = "win";
+			}elseif(stripos($uname, "Linux") !== false){
+				if(@file_exists("/system/build.prop")){
+					self::$os = "android";
+				}else{
+					self::$os = "linux";
+				}
+			}elseif(stripos($uname, "BSD") !== false or $uname === "DragonFly"){
+				self::$os = "bsd";
+			}else{
+				self::$os = "other";
+			}
+		}
+
+		return self::$os;
+	}
+}
+
 function kill($pid){
 	switch(CommandReader::getOS()){
 		case "win":
@@ -110,4 +231,16 @@ function kill($pid){
 		default:
 			exec("kill -9 " . ((int) $pid) . " > /dev/null 2>&1");
 	}
+}
+
+function console($msg, $ret = null, $eol = true){
+	echo date("[H:i:s] ") . $msg . ($eol ? PHP_EOL : "");
+	return $ret;
+}
+
+$cmdReader = new CommandReader();
+
+function nonBlockReadLine(){
+	global $cmdReader;
+	return $cmdReader->getLine();
 }
--- php/wrapper.php
+++ php/wrapper.php
@@ -20,7 +20,7 @@ if(!isset($ok, $target)){
 
 $args = isset($argv) ? $argv :
 	console("[WARNING] Not passing any command line arguments to PocketMine server. Please enable 'register_argc_argv' in " .
-	php_ini_loaded_file() . " if you want this script to pass your arguments into PocketMine.", []);
+		php_ini_loaded_file() . " if you want this script to pass your arguments into PocketMine.", []);
 if(is_file($args[0]) and realpath($args[0]) === realpath(__FILE__)){
 	array_shift($args);
 }
@@ -31,7 +31,9 @@ console("[?] Please enter the number of times to restart the server (including t
 console("[?] Enter 1 if you don't want the server to restart.");
 console("[?] Enter 0 if you want the server to restart for infinite times.");
 
-while(($line = nonBlockReadLine()) === null);
+while(($line = nonBlockReadLine()) === null){
+	;
+}
 if(!is_numeric($line)){
 	console("[ERROR] Invalid input!");
 	goto request_enter_num_times_restart;
@@ -41,14 +43,17 @@ $restarts = ($restarts === 0) ? PHP_INT_MAX : $restarts;
 
 while($restarts--){
 	console("Initializing server...");
-	$server = proc_open(PHP_BINARY . " " . implode(" ", $args), [0 => ["pipe", "r"], 1 => ["pipe", ,"w"], 2 => fopen("php://stderr", "wb")], $pipes);
+	$server = proc_open(PHP_BINARY . " " . implode(" ", $args), [0 => ["pipe", "r"], 1 => ["pipe", "w"], 2 => fopen("php://stderr", "wb")], $pipes);
 	if(!is_resource($server)){
 		console("[ERROR] Failed to start server once :(");
 		continue;
 	}
 	console("[*] The server has been started. Type '-die' to stop the server and prevent this script from restarting. Type '-kill' to terminate the server and stop this script.");
+	$reader = new StreamReader($pipes[1]);
 	while(proc_get_status($server)["running"]){
-		echo stream_get_contents($pipes[1]);
+		while($line = $reader->getLine()){
+			echo $line . PHP_EOL;
+		}
 		while(($line = nonBlockReadLine()) !== null){
 			if(strtolower($line) === "-die"){
 				$restarts = 0;
@@ -60,30 +65,42 @@ while($restarts--){
 					fclose($pipes[2]);
 				}
 				$pid = proc_get_status($server)["pid"];
+				kill($pid);
+				console("Server killed");
 				proc_terminate($server);
-				switch(CommandReader::getOS()){
-					case "win":
-						$tasks = `umic process where (ParentProcessId=$pid) get ProcessId`;
-						foreach(explode(PHP_EOL, $tasks) as $child){
-							if(is_numeric($child)) kill($child);
-						}
-						break;
-					default:
-						exec("kill -9 -\$(ps -o pgid= $pid | grep -o '[0-9]*')");
-						break;
-				}
+				console("Process terminated.");
+//				switch(CommandReader::getOS()){
+//					case "win":
+//						$tasks = `umic process where (ParentProcessId=$pid) get ProcessId`;
+//						foreach(explode(PHP_EOL, $tasks) as $child){
+//							if(is_numeric($child)){
+//								kill($child);
+//							}
+//						}
+//						break;
+//					default:
+//						exec("kill -9 -\$(ps -o pgid= $pid | grep -o '[0-9]*')");
+//						break;
+//				}
 				proc_close($server);
-				exit;
+				console("Process closed.");
+				exit(0);
 			}
 			fwrite($pipes[0], $line . PHP_EOL);
 		}
 	}
 	echo stream_get_contents($pipes[1]);
+	$reader->running = false;
+	$reader->join();
 	fclose($pipes[1]);
 	fclose($pipes[0]);
 	if(isset($pipes[2])){ // I am not sure if it exists
 		fclose($pipes[2]);
 	}
-	proc_close($pipes[2]);
+	if(is_resource($server)){
+		proc_close($server);
+	}
 	console("[*] The server has been stopped.");
 }
+console("Finished looping.");
+exit(0);
